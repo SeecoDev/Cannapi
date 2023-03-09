5532bb54bc60b27e20a1b9904581e36b6e513b0a
SeecoDev <floresmenaadrian@gmail.com>
2023-03-08 22:03:15 -0800

    Se creo el /about

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def main():
-    return "HOLA ITE"
+    return "<center><h1>HOLA ITE</h1></center><marquee>ADRIAN CHAD</marquee>"
+
+@app.route("/about")
+def about():
+    with open('cannapi.json',"r") as json:
+        display = json.read()   
+        return display
+    
 
 @app.route("/bye")
 def adios():
-    return "Hasta luego putitos"
+    return "<h1> Recuerden fumar bandita</h1>"
+
