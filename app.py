269120dd5959ba586d1a01f54fb41b0e299cc757
TonyVazz <al19760723@ite.edu.mx>
2023-03-12 23:38:43 -0700

    Agregue el select from al codigo para mostrar los datos en un endpoint

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify
-import json
+import json;
+import mysql.connector;
+
+
 
 app = Flask(__name__)
 
@@ -7,16 +10,44 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<center><h1>HOLA ITE</h1></center><marquee>ADRIAN CHAD</marquee>"
+    return "<center><h1>HOLA ITE</h1></center><marquee>ADRIAN Y TONY CHAD</marquee>"
 
 @app.route("/about")
 def about():
     with open('cannapi.json',"r") as json:
         display = json.read()   
-        return display
+        return jsonify(display)
 
 
 @app.route("/bye")
 def adios():
     return "<h1> Recuerden fumar bandita</h1>"
+
+
+@app.route("/api/sql/test")
+def database():
+    connection = mysql.connector.connect(host='localhost',
+                                         database='cannabis',
+                                         user='root',
+                                         password='Tony12345')
+    
+    sql_select_Query = "select * from flor"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    # get all records
+    records = cursor.fetchall()
+    
+    datos_json = []
+    for row in records:
+        datos_json.append({
+            'idFlor = ': row[0], 
+            'nameFlor = ': row[1],
+            'varietyFlor  = ': row[2],
+            'cultivoFlor  = ': row[3]
+        })
+        
+    connection.close()
+    return jsonify(datos_json)
+
+
 
