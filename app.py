b3168afce6687f6adc23862dd11daa34596edeff
SeecoDev <floresmenaadrian@gmail.com>
2023-03-16 19:11:17 -0700

    Se subio el script de la base de datos

--- app.py
+++ app.py
@@ -20,45 +20,40 @@ def about():
 
 
 @app.route("/bye")
-def adios():
+def bye():
     return "<h1> Recuerden fumar bandita</h1>"
 
-
-@app.route("/api/sql/test")
-def database():
+@app.route("/api/sql/select")
+def select():
     connection = mysql.connector.connect(host='localhost',
-                                         database='cannabis',
-                                         user='root',
-                                         password='Tony12345')
+                                         database='cannapi',
+                                         user='deuz',
+                                         password='2Tostitos3!')
     
     sql_select_Query = "select * from flor"
     cursor = connection.cursor()
     cursor.execute(sql_select_Query)
-    # get all records
     records = cursor.fetchall()
-    
     datos_json = []
     for row in records:
         datos_json.append({
-            'idFlor = ': row[0], 
-            'nameFlor = ': row[1],
-            'varietyFlor  = ': row[2],
-            'cultivoFlor  = ': row[3],
-            'ctchFlor  = ': row[4],
-            'cdbFlor  = ': row[5],
-            'terpenesFlor  = ': row[6],
-            'efectsFlor  = ': row[7],
-            'tasteFlor  = ': row[8],
-            'dateFlor  = ': row[9],
-            'providerFlor  = ': row[10],
-            'colorsFlor  = ': row[11],
-            'awardsFlor  = ': row[12],
-            'priceFlor  = ': row[13],
-            'weightFlor  = ': row[14]
+            'florId = ': row[0], 
+            'florNombre = ': row[1],
+            'florTipo  = ': row[2],
+            'florCreacion  = ': row[3],
+            'florCreador  = ': row[4],
+            'florPadres  = ': row[5],
+            'florTHC  = ': row[6],
+            'florCBD  = ': row[7],
+            'florPremios  = ': row[8],
+            'florDescripcion  = ': row[9],
+            'florEfectos  = ': row[10],
+            'florSabores  = ': row[11]
         })
         
     connection.close()
     return jsonify(datos_json)
 
+    
 
 
